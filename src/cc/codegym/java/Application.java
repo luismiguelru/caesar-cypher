951a835a8e4c434d334d176e805d2432a547e9f8
luismiguelru <luismiguelru@hotmail.com>
2022-08-03 18:08:43 +0200

    Testing regex implementation

--- src/cc/codegym/java/Application.java
+++ src/cc/codegym/java/Application.java
@@ -3,14 +3,18 @@ package cc.codegym.java;
 public class Application {
     public static void main(String[] args) {
         CaesarCypher cypher = new CaesarCypher(3);
-        String originalMessage = "Java";
+        String originalMessage = " Java";
         String cypheredMessage = cypher.cypher(originalMessage);
         String finalMessage = cypher.decypher(cypheredMessage);
+
+        String cypheredMessageTwo = cypher.cypherTwo(originalMessage);
 
         System.out.println(originalMessage);
         System.out.println(cypheredMessage);
         System.out.println(finalMessage);
         System.out.println(originalMessage.equals(finalMessage));
+        System.out.println();
+        System.out.println(cypheredMessageTwo);
 
 
     }
--- src/cc/codegym/java/CaesarCypher.java
+++ src/cc/codegym/java/CaesarCypher.java
@@ -1,17 +1,36 @@
 package cc.codegym.java;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CaesarCypher {
 
     private final int cypherKey;
+    //Regex Alphabetic Character
+    private static final String PATTERN = "^[.,!¡¿?-a-zA-Z]*$";
+    //RegexDoubleQuotationMarks
+    private static final String PATTERNTWO = "[^\"]*\"|\\w*";
+    //RegexWhitespace
+    private static final String PATTERNTHREE = "\\p{Zs}";
+
+
+
 
     public CaesarCypher(int cypherKey) {
         this.cypherKey = cypherKey;
     }
 
     public   String cypher(String text){
+
+
         StringBuilder cypheredMessage = new StringBuilder();
         for (int i =0 ; i < text.length() ; i++){
             char c = (char)(text.charAt(i)+ cypherKey);
+
             cypheredMessage.append(c);
         }
 
@@ -26,4 +45,26 @@ public class CaesarCypher {
 
         return cypheredMessage.toString();
     }
+
+    public String cypherTwo (String text){
+        StringBuilder cypheredMessage = new StringBuilder();
+        String copy = text;
+        char[] copyChar =  copy.toCharArray();
+        Character[] charArrBoxed = new String(copyChar).chars()
+                .mapToObj(c ->(char)c)
+                .toArray(Character[]::new);
+
+
+         for (int i = 0; i < charArrBoxed.length ; i++){
+            if ( charArrBoxed[i].toString().matches(PATTERN) || charArrBoxed[i].toString().matches(PATTERNTWO)
+            || charArrBoxed[i].toString().matches(PATTERNTHREE))
+            {
+                char ca = (char)(text.charAt(i)+ cypherKey);
+                cypheredMessage.append(ca);
+             }
+
+            }
+
+        return cypheredMessage.toString();
+         }
 }
